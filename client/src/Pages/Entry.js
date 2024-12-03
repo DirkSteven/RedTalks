cd77d4790bbcfba5bac36adea2653d33f90df3e0
Mikaella Ebora <129576196+mikaebora@users.noreply.github.com>
2024-12-03 14:26:29 +0800

    cover

--- client/src/Pages/Entry.js
+++ client/src/Pages/Entry.js
@@ -1,10 +1,11 @@
 import './css/landing.css';
 import { Outlet } from 'react-router-dom';
+import coverImg from '../Assets/cover.png';
 
 function Entry() {
   return (
     <div className='formcontainer'>
-      <img alt='FlavorImg' src=''></img>
+      <img alt='FlavorImg' src={coverImg}></img>
       <div className='panel'>
         <Outlet/>
       </div>
